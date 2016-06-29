577c0ae423603329116a4c05d016f22bb286a5e9
Matthew Zimmerman <matt.zimmerman@ccri.com>
2016-06-29 12:11:31 -0400

    Implemented solutions for pascal and balance.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,32 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (r == 0 && c == 0) 1
+    else if (c < 0 || c > r) 0
+    else pascal(c-1, r-1)+pascal(c, r-1)
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], level: Int): Int = {
+      if (chars.isEmpty)
+        level
+      else {
+        chars.head match {
+          case '(' => f(chars.tail, level+1)
+          case ')' => if (level == 0) -1 else f(chars.tail, level-1)
+          case _ => f(chars.tail, level)
+        }
+      }
+    }
+    f(chars, 0) == 0
+  }
   
   /**
    * Exercise 3
    */
     def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+}
